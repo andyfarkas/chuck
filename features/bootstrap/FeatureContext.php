b178bd37c9d6e62f9fb3d9d63ceb4c98ab25bb84
Michal Svec <pan.svec@gmail.com>
2014-04-02 09:09:17 +0200

    Context file cleanup

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -7,30 +7,11 @@ use Behat\Behat\Context\ClosuredContextInterface,
 use Behat\Gherkin\Node\PyStringNode,
     Behat\Gherkin\Node\TableNode;
 
-//
-// Require 3rd-party libraries here:
-//
-//   require_once 'PHPUnit/Autoload.php';
-//   require_once 'PHPUnit/Framework/Assert/Functions.php';
-//
-
 /**
  * Features context.
  */
 class FeatureContext extends \Behat\MinkExtension\Context\MinkContext
 {
-    /**
-     * Initializes context.
-     * Every scenario gets its own context object.
-     *
-     * @param array $parameters context parameters (set them up through behat.yml)
-     */
-    public function __construct(array $parameters)
-    {
-        // Initialize your context here
-    }
-
-
     /**
      * @Given /^I am on "([^"]*)" svn log page$/
      */
